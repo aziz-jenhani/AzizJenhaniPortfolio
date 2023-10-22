abbffc79f610adb3905b6aba311b876ee61fa124
Aziz <azizjenhani11@gmail.com>
2023-10-22 01:00:13 +0100

    sers commit6

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+/*
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: "binary-loader",
+    });
+
+    return config;
+  },
+};
+*/
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  reactStrictMode: true,
+
+  // Add basePath
   images: {
     loader: 'custom',
     loaderFile: './app/my-loader.ts',
